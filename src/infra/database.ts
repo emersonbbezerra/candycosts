0bf54d5e50641cc94ca7cf07a3df0a0e00133a01
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-05-19 16:32:34 -0300

    feat: remove jest and add .env

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function connect() {
+  const databaseUrl = process.env.DATABASE_URL;
+  if (!databaseUrl) {
+    throw new Error("A variável de ambiente DATABASE_URL não está definida.");
+  }
   try {
-    await mongoose.connect(
-      "mongodb+srv://emersonbbezerra:QKo1XpHnoV0esJ0X@confeitaria2.qfq4q0q.mongodb.net/confeitaria"
-    );
+    await mongoose.connect(databaseUrl);
     console.log("Connect database success");
   } catch (error) {
     console.log("~ file: database.ts:5 ~ connect ~ error:", error);
